62d86ec2d818de883db16994f8e0b376af71e23d
Alper Kaan Dalarslan <110106760+AlperKaanDalaraslan@users.noreply.github.com>
2023-05-02 13:24:05 +0300

    ana sayfa eklendi

---
+++ resources/views/anasayfa.blade.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+
+    <title>e Pati</title>
+    <link type="text/css" rel="stylesheet" href="/css/anasayfa.css">
+
+</head>
+
+<body>
+<section class="main">
+    <nav>
+        <a href="index.html" class="logo">
+            <img src="/images/logo.png">
+        </a>
+
+        <ul>
+            <li>
+                <a href="">
+                    <p class="menuyazisi">VETERİNER RANDEVU </p></p>
+                </a>
+            </li>
+
+            <li>
+                <p class="menucizgisi"></p>
+            </li >
+
+            <li>
+                <a href="">
+                    <p class="menuyazisi"> HAYVAN SAHİPLENME </p>
+                </a>
+            </li>
+
+            <li>
+                <p class="menucizgisi"></p>
+            </li>
+
+            <li>
+                <a href="">
+                    <p class="menuyazisi"> KAYIP İLANI </p>
+                </a>
+            </li>
+
+            <li>
+                <p class="menucizgisi"></p>
+            </li>
+
+            <li>
+                <a href="">
+                    <p class="menuyazisi"> EŞ BULMA</p>
+                </a>
+            </li>
+        </ul>
+
+        <a href=""> <img src="/images/kayitol.png" class="kayitol-girisyap"> </a>
+        <a href=""> <img src="/images/girisyap.png" class="kayitol-girisyap" > </a>
+    </nav>
+
+
+
+    <nav class="reklam">
+        <div>
+                    <pre class="reklamyazisi"><font color="white"> Canlarını kurtarmak
+                        için,
+                        onların </font> <font color="329ce0">sesi ol!</font>
+                    </pre>
+            <img class="reklamresmi" src="/images/reklamresmi.png">
+        </div>
+    </nav >
+
+
+
+    <nav class="bagis">
+        <div>
+                    <pre class="bagisyazi1" >Haydi!!!
+Sende bir bağışta bulun.
+Onlara umut ol.</pre>
+            <div class="logo_yazi_duzenleme">
+
+            <img class="epati_logo" src="/images/e_pati_logolu.png" >
+            <pre class="bagisyazi3">e-pati gündemini takip edin
+            </pre>
+
+            </div>
+        </div>
+
+        <div>
+            <div>
+
+    </nav>
+
+
+    <nav class="hizmet">
+
+        <pre class="hizmet_yazisi"><h1>e-Pati Yapılan Hizmetler</h1></pre>
+
+        <div>
+            <img src="/images/randevu_foto.png" class="hizmetimage" >
+
+            <div class="card-body">
+                <h2 class="hizmet_yazisi2">-</h2>
+                <h2 class="hizmet_yazisi2">Gerçekleştirilen</h2>
+                <h2 class="hizmet_yazisi2" >Randevu</h2>
+            </div>
+        </div>
+
+        <div>
+            <img src="/images/sahiplenme_foto.png" class="hizmetimage" >
+
+            <div class="card-body">
+                <h2 class="hizmet_yazisi2">-</h2>
+                <h2 class="hizmet_yazisi2">Sahiplenilen</h2>
+                <h2 class="hizmet_yazisi2" >Hayvan</h2>
+            </div>
+        </div>
+
+        <div>
+            <img src="/images/kayipfoto.png" class="hizmetimage" >
+
+            <div class="card-body">
+                <h2 class="hizmet_yazisi2">-</h2>
+                <h2 class="hizmet_yazisi2">Bulunan</h2>
+                <h2 class="hizmet_yazisi2" >Kayıplar</h2>
+            </div>
+        </div>
+
+        <div>
+            <img src="/images/esfoto.png" class="hizmetimage" >
+
+            <div class="card-body">
+                <h2 class="hizmet_yazisi2">-</h2>
+                <h2 class="hizmet_yazisi2">Bulunan eş</h2>
+                <h2 class="hizmet_yazisi2" >Adayları</h2>
+            </div>
+        </div>
+
+    </nav>
+
+
+    <nav class="footer ">
+
+
+
+        <div class="footerduzenleme">
+            <div class="footerduzenlemeic">
+                <h1 class="footerbaslik">BİZ KİMİZ ?</h1>
+                <hr>
+                <pre class="footeryazisi">Biz hayvanseverler topluluğu için kar amacı gütmeyen
+bir platform oluşturuyoruz. Bu platformda
+veteriner randevuları, hayvan sahiplendirme,
+kayıp ilanları ve hayvanlara yardım için bağış toplama
+gibi hizmetler sunuyoruz. Amacımız, hayvanseverlerin
+tüm ihtiyaçlarını tek bir yerden karşılamalarını sağlamak.
+                        </pre>
+            </div>
+        </div>
+        <div class="footerduzenleme">
+            <div class="footerduzenlemeic">
+                <h1 class="footerbaslik">İLETİŞİM</h1>
+                <hr c>
+                <pre class="footeryazisi"><a href=""><img src="/images/email.png" ></a>  e_pati@gmail.com
+
+<a href=""><img src="/images/phone-call.png" ></a>  0555 555 55 55
+
+<a href=""><img src="/images/pin.png" ></a>  Üniversite, 23200 Elâzığ Merkez/Elazığ
+                        </pre>
+            </div>
+        </div>
+
+        <div class="footerduzenleme">
+            <div class="footerduzenlemeic">
+                <h1 class="footerbaslik">BİZİ TAKİP EDİN</h1>
+                <hr>
+                <pre class="footeryazisi"><a href=""><img src="/images/facebook.png" ></a>               <a href=""><img src="/images/instagram.png" ></a>              <a href=""><img src="/images/twitter.png" ></a>              <a href=""><img src="/images/linkedin.png" ></a>
+                        </pre>
+            </div>
+        </div>
+
+        <div class="footerduzenleme">
+            <div class="footerduzenlemeic">
+                <h1 class="footerbaslik">BAĞIŞTA BULUN</h1>
+                <hr class="cizgikonum">
+                <pre class="footeryazisi">Hayvanlar da bizim
+gibi sevgi dolu
+varlıklardır, onların
+mutluluğu ve sağlığı da
+bizim sorumluluğumuzdadır.
+Lütfen onları sevin,
+koruyun ve hayatlarına
+dokunacak bir <a href="---------------" style="color:yellow">bağışta bulunun.</a>
+                        </pre>
+            </div>
+        </div>
+
+
+
+    </nav>
+
+</section>
+
+
+
+</body>
+
+
+
+</html>
