0da82d6cbdf693666c033989757c8d70d89eb1d4
Ekrem Kavak <ekremkavak42@gmail.com>
2023-05-03 16:08:05 +0300

    css hataları #1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,20 +2,21 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>e Pati | @yield('title')</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
 
     <link href="/css/anasayfa.css" rel="stylesheet">
 
+    @yield('css_js')
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
 </head>
 <body>
 <section class="main">
@@ -157,6 +158,5 @@ dokunacak bir <a href="---------------" style="color:yellow">bağışta bulunun.
     </nav>
 
 </section>
-
 </body>
 </html>
