4813a27c58355bd36240ae8394eab22d2f0e0550
Alper Kaan Dalarslan <110106760+AlperKaanDalaraslan@users.noreply.github.com>
2023-05-22 14:04:36 +0300

    #Kayip ilan sayfasi
    kayip ilan yonlendirmeler controller ve kayip kart işlemleri

--- app/Http/Controllers/KayipController.php
+++ app/Http/Controllers/KayipController.php
@@ -3,9 +3,61 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Sahiplen;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class KayipController extends Controller
 {
+    public function create(Request $request){
+
+        $request->validate([
+            'hayvan_foto'=>'required',
+            'baslik' => 'required',
+            'tur'=>'required',
+            'cinsiyet'=>'required',
+            'kayip'=>'required',
+            'bulunan'=>'required',
+            'il_id'=>'required',
+            'ilce'=>'required',
+            'adres'=>'required',
+            'aciklama'=>'required',
+
+        ]);
+
+
+
+        $data = new Kayip();
+        $data->user_id = Auth::id();
+        $data->tur = $request->tur;
+        $data->cinsiyet = $request->cinsiyet;
+        $data->kayip = $request->kayip;
+        $data->bulunan = $request->bulunan;
+        $data->cip_no = $request->cip;
+        $data->il_id = $request->il_id;
+        $data->ilce = $request->ilce;
+        $data->adres = $request->adres;
+        $data->baslik = $request->baslik;
+        $data->aciklama = $request->aciklama;
+
+
+        $data->save();
+
+        return redirect()->route('kayip_ilan_form');
+
+    }
+    public function index(){
+        $data = Sahiplen::paginate(5);
+        return view('kayip_ilan_sayfasi',compact('data'));
+
+    }
+
+    public function show($id){
+        $data =Sahiplen::find($id);
+        return view('kayip_hayvan',compact('data'));
+    }
+
+    }
     //
 }
--- app/Models/Kayip.php
+++ app/Models/Kayip.php
@@ -8,5 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Kayip extends Model
 {
     use HasFactory;
+    protected $table = 'kayip';
+
+    protected $fillable = [
+        'hayvan_image',
+        'tur',
+        'cinsiyet',
+        'kayip',
+        'bulunan',
+        'cip_no',
+        'il_id',
+        'ilce',
+        'adres',
+        'aciklama',
+    ];
+
+
+
+    public function getUser(){
+        return $this->belongsTo(User::class,'user_id','id');
+    }
+    public function getIL(){
+        return $this->belongsTo(il::class,'il_id','id');
+    }
 
 }
