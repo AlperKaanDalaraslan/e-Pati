541b4de698c4ec9297fb4103b7ca8f760579e91a
Ekrem Kavak <ekremkavak42@gmail.com>
2023-05-24 07:21:49 +0300

    log_in sayfası veteriner giriş
    log in ekranı css düzenlemesi yapıldı ve veteriner giriş ekranı eklendi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,76 +1,120 @@
 @extends('layouts.app')
 @section('css_js')
 
+    <link rel="stylesheet" href="/css/giris.css" /> <!-- bu css dosyasını okumuyor !!!!!!!!-->
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+        height: 100vh;
+        width: 100vw;
+        background-color: #ffffff;
+    }
+
+    .giris_navbar a {
+        font-size: 30px;
+        font-family: "Microsoft PhagsPa";
+        line-height: 0.5;
+        float: left;
+        display: block;
+        color: black;
+        text-align: center;
+        padding: 20px 20px;
+        text-decoration: none;
+    }
+
+    .login-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        margin-bottom: 50px;
+    }
+    .sign_up_kismi {
+        font-family: "Nunito Sans";
+        margin-top: 20px;
+        text-align: center;
+    }
+
+
+    form {
+        width: 350px;
+        margin: 0 20px;
+        padding: 30px;
+        border: 1px solid #ddd;
+        border-radius: 10px;
+        background-color: #f7f7f7;
+    }
+
+    h2 {
+        margin-bottom: 20px;
+        text-align: center;
+    }
+
+    label {
+        display: block;
+        margin-bottom: 5px;
+    }
+
+    input[type="text"],
+    input[type="password"] {
+        width: 100%;
+        padding: 10px;
+        margin-bottom: 20px;
+        border: 1px solid #ddd;
+        border-radius: 5px;
+    }
+
+    .blue-button {
+        padding: 10px 20px;
+        background-color: #007bff;
+        color: #fff;
+        border: none;
+        border-radius: 5px;
+        font-size: 18px;
+    }
+
+    a {
+        color: #009688;
+        text-decoration: none;
+    }
+
+
+</style>
 
 @endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
+    <div class="login-container">
+        <form method="POST" action="{{ route('login') }}" class="user-login">
+            @csrf
+            <h2>Üye Girişi</h2>
+            <label for="username">Kullanıcı email</label>
+            <input type="text" id="email"  class="form-control @error('email') is-invalid @enderror" name="email" value="{{old('email')}}" required autocomplete="email" autofocus>
+            @error('email')
+            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+            <label for="password">Şifre:</label>
+            <input type="password" id="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+            @error('password')
+            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
+            @enderror
+            <button class="blue-button" type="submit">Giriş Yap</button>
+            <div class="sign_up_kismi"> Hesabınız yok mu? <a href="{{'register'}}"> Hesap Oluştur</a></div>
+        </form>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+        <form class="vet-login">
+            <h2>Veteriner Girişi</h2>
+            <label for="clinicname">Kullanıcı email</label>
+            <input type="text" id="email" name="email" required>
+            <label for="password">Şifre:</label>
+            <input type="password" id="password" name="password" required>
+            <button class="blue-button" type="submit">Giriş Yap</button>
+            <div class="sign_up_kismi">Yeni veteriner misiniz? <a href="#">Veteriner hesabı Oluştur</a></div>
+        </form>
     </div>
-</div>
+
 @endsection
